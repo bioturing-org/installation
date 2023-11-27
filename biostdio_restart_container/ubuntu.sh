0786467193ba2f0c1493ec9d96abb78a35589ac2
Lalit-shaktawat <lalit@bioturing.com>
2023-11-27 18:02:35 +0700

    Bioproxy version updated

--- biostdio_restart_container/ubuntu.sh
+++ biostdio_restart_container/ubuntu.sh
@@ -225,9 +225,9 @@ fi
 
 # Check Version
 echo -e "\n"
-read -p "Please enter Biocolab's Proxy 1.0.25 (latest): " COLAB_PROXY_VERSION
+read -p "Please enter Biocolab's Proxy 1.0.26 (latest): " COLAB_PROXY_VERSION
 if [ -z "$COLAB_PROXY_VERSION" ]; then
-    COLAB_PROXY_VERSION="1.0.25"
+    COLAB_PROXY_VERSION="1.0.26"
 fi
 
 echo -e "\n HTTP_SERVER_PORT : $HTTP_PORT"
