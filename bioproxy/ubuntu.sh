4ff7b2a7e765440ff2e1c4a3a0a7a89e151aca5e
Pham Thai Hoa <thaihoabo@gmail.com>
2024-03-20 12:37:44 +0700

    Update ubuntu.sh

--- bioproxy/ubuntu.sh
+++ bioproxy/ubuntu.sh
@@ -181,9 +181,9 @@ fi
 
 # Check Version
 echo -e "\n"
-read -p "Please enter Biocolab's Proxy 1.0.27 (latest): " COLAB_PROXY_VERSION
+read -p "Please enter Biocolab's Proxy 1.0.28 (latest): " COLAB_PROXY_VERSION
 if [ -z "$COLAB_PROXY_VERSION" ]; then
-   COLAB_PROXY_VERSION="1.0.27"
+   COLAB_PROXY_VERSION="1.0.28"
    echo -e "\nBioproxy Version: $COLAB_PROXY_VERSION\n"
 
 fi
